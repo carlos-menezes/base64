714e4abbcbb8f0f0dd530e0e977a3eac14fa2493
Carlos Menezes <carlos-menezes212@hotmail.com>
2022-03-07 19:22:21 +0000

    Add test vectors from RFC 4648

--- src/lib.rs
+++ src/lib.rs
@@ -1,25 +1,49 @@
-mod base64;
+pub mod base64;
 mod err;
 
 #[cfg(test)]
 mod tests {
-    use crate::base64::base64_encode;
+    use crate::base64::encode;
 
     #[test]
-    fn test_len7() {
-        let result = base64_encode("light w").unwrap();
-        assert_eq!(result, "bGlnaHQgdw==");
+    fn test_encode_empty() {
+        let result = encode("").unwrap();
+        assert_eq!(result, "");
     }
 
     #[test]
-    fn test_len8() {
-        let result = base64_encode("light wo").unwrap();
-        assert_eq!(result, "bGlnaHQgd28=");
+    fn test_encode_f() {
+        let result = encode("f").unwrap();
+        assert_eq!(result, "Zg==");
     }
 
     #[test]
-    fn test_len9() {
-        let result = base64_encode("light wor").unwrap();
-        assert_eq!(result, "bGlnaHQgd29y");
+    fn test_encode_fo() {
+        let result = encode("fo").unwrap();
+        assert_eq!(result, "Zm8=");
+    }
+
+    #[test]
+    fn test_encode_foo() {
+        let result = encode("foo").unwrap();
+        assert_eq!(result, "Zm9v");
+    }
+
+    #[test]
+    fn test_encode_foob() {
+        let result = encode("foob").unwrap();
+        assert_eq!(result, "Zm9vYg==");
+    }
+
+    #[test]
+    fn test_encode_fooba() {
+        let result = encode("fooba").unwrap();
+        assert_eq!(result, "Zm9vYmE=");
+    }
+
+    #[test]
+    fn test_encode_foobar() {
+        let result = encode("foobar").unwrap();
+        assert_eq!(result, "Zm9vYmFy");
     }
 }
